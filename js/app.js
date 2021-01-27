0a662beb65d0f47cad424c947bc0a5eae058ca2a
Brian Lemons <brian_lemons@hotmail.com>
2021-01-26 19:58:20 -0900

    lab setup

--- js/app.js
+++ js/app.js
@@ -2,14 +2,3 @@
 
 let hours = ['6am', '7am', '8am'];
 
-let seattle = {
-  name: 'Seattle',
-  min: 23,
-  max: 65,
-  avg: 6.3,
-  // a method to calculate random number of customers per hour
-  // generate a method to calculate and populate our number of cookies sold per hour array
-  // a method to render the list items
-};
-
-
