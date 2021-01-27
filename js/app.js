05d80aef04385bb1428c47de62f1671deb202eed
Brian Lemons <brian_lemons@hotmail.com>
2021-01-26 20:42:41 -0900

    lab setup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,41 @@
 'use strict';
 
-let hours = ['6am', '7am', '8am'];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+let seattle = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  // a method to calculate random number of customers per hour
+  // generate a method to calculate and populate our number of cookies sold per hour array
+  // a method to render the list items
+};
+
+let tokyo = {
+  name: 'Tokyo',
+  min: 3,
+  max: 24,
+  avg: 1.2,
+};
+
+let dubai = {
+  name: 'Dubai',
+  min: 11,
+  max: 38,
+  avg: 3.7,
+};
+
+let paris = {
+  name: 'Paris',
+  min: 20,
+  max: 38,
+  avg: 2.3,
+};
+
+let lima = {
+  name: 'Lima',
+  min: 2,
+  max: 16,
+  avg: 4.6,
+}
